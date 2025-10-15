95bf5a6aed6932026d25197f14782465c44c3bb4
achhishiksha <pardhandhamu@gmail.com>
2025-10-15 19:54:39 +0530

    port changes to didgitocen

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ DB_USER = os.getenv("DB_USER") or os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+# DB_PORT = int(os.getenv("DB_PORT", 3306)),
+DB_PORT=int(os.getenv("DB_PORT"))
+
 
 if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_NAME]):
     raise SystemExit("Missing required DB environment variables (DB_USER/DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)")
